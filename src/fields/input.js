1d05ea000d4a0b3c528a1416ac39bbfd37680a4a
Lukas Martinelli <me@lukasmartinelli.ch>
2016-12-17 20:21:24 +0100

    No text selection for labels

--- src/fields/input.js
+++ src/fields/input.js
@@ -3,41 +3,42 @@
 import theme from '../theme.js'
 
 const base = {
-		display: 'inline-block',
-		fontSize: theme.fontSizes[5],
-		lineHeight: 2,
-		paddingLeft: 5,
-		paddingRight: 5,
+  display: 'inline-block',
+  fontSize: theme.fontSizes[5],
+  lineHeight: 2,
+  paddingLeft: 5,
+  paddingRight: 5,
 }
 
 const label = {
-	...base,
-	width: '40%',
-	color: theme.colors.lowgray,
+  ...base,
+  width: '40%',
+  color: theme.colors.lowgray,
+  userSelect: 'none',
 }
 
 const property = {
-	marginTop: theme.scale[2],
-	marginBottom: theme.scale[2],
+  marginTop: theme.scale[2],
+  marginBottom: theme.scale[2],
 }
 
 const input = {
-	...base,
-	border: '1px solid rgb(36, 36, 36)',
-	width: '47%',
-	backgroundColor: theme.colors.gray,
-	color: theme.colors.lowgray,
+  ...base,
+  border: '1px solid rgb(36, 36, 36)',
+  width: '47%',
+  backgroundColor: theme.colors.gray,
+  color: theme.colors.lowgray,
 }
 
 const select = {
-	...input,
-	width: '51%',
-	height: '2.3em',
+  ...input,
+  width: '51%',
+  height: '2.3em',
 }
 
 export default {
-	label,
-	select,
-	input,
-	property,
+  label,
+  select,
+  input,
+  property,
 }
