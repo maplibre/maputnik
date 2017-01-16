3b7fb7ae75c88f22bd0489a00d6fd5261618ebee
jirik <jiri.kozel.cz@gmail.com>
2017-01-16 14:58:53 +0100

    Fix checkbox not showing status

--- src/components/inputs/CheckboxInput.jsx
+++ src/components/inputs/CheckboxInput.jsx
@@ -17,7 +17,9 @@ class CheckboxInput extends React.Component {
         checked={this.props.value}
       />
       <div className="maputnik-checkbox-box">
-        <svg className="maputnik-checkbox-icon" viewBox='0 0 32 32'>
+        <svg style={{
+            display: this.props.value ? 'inline' : 'none'
+          }} className="maputnik-checkbox-icon" viewBox='0 0 32 32'>
           <path d='M1 14 L5 10 L13 18 L27 4 L31 8 L13 26 z' />
         </svg>
       </div>
