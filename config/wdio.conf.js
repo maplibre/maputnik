58a5b549be1c8e71222a8394a52dee39c3f41dc0
orangemug <info@orangemug.co.uk>
2017-11-15 18:08:43 +0000

    Moved back to testing with phantom

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -1,6 +1,6 @@
 var webpack          = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
-var webpackConfig    = require("./webpack.config");
+var webpackConfig    = require("./webpack.production.config");
 var testConfig       = require("../test/config/specs");
 var isDocker         = require("is-docker");
 var path             = require("path");
@@ -21,19 +21,19 @@ exports.config = {
   maxInstances: 10,
   capabilities: [{
     maxInstances: 5,
-    browserName: 'chrome'
+    browserName: 'firefox'
   }],
   sync: true,
   logLevel: 'verbose',
   coloredLogs: true,
   bail: 0,
   screenshotPath: SCREENSHOT_PATH,
-  host: (isDocker() ? process.env.DOCKER_HOST : "127.0.0.1"),
   baseUrl: 'http://localhost',
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
   framework: 'mocha',
+  services: ['phantomjs'],
   reporters: ['spec'],
   phantomjsOpts: {
     webdriverLogfile: 'phantomjs.log'
@@ -49,12 +49,6 @@ exports.config = {
     server.listen(testConfig.port);
   },
   onComplete: function(exitCode) {
-    server.close();
+    server.close()
   }
-}
-
-if(!isDocker()) {
-  exports.config.services = [
-    'selenium-standalone'
-  ]
 }
