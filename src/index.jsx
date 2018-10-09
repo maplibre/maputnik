843d3df8bc17fbed1e2f6509c544c6a88141ef9c
orangemug <info@orangemug.co.uk>
2018-10-09 20:58:14 +0100

    Fixed react-icons size/position.

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import { IconContext } from "react-icons";
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -5,4 +6,9 @@ import './favicon.ico'
 import './styles/index.scss'
 import App from './components/App';
 
-ReactDOM.render(<App/>, document.querySelector("#app"));
+ReactDOM.render(
+  <IconContext.Provider value={{className: 'react-icons'}}>
+    <App/>
+  </IconContext.Provider>,
+  document.querySelector("#app")
+);
