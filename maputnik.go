2e58be1c90e0bec41b256d8b7a01492ade56261e
Lukas Welte <info@lukaswelte.de>
2019-02-09 13:47:58 +0100

    Switch around editor and desktop in version field so that it prints nicer

--- maputnik.go
+++ maputnik.go
@@ -16,7 +16,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "maputnik"
 	app.Usage = "Server for integrating Maputnik locally"
-	app.Version = "Desktop: 1.0.3; Editor: 1.5.0"
+	app.Version = "Editor: 1.5.0; Desktop: 1.0.3"
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
