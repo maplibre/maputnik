ef81534a1726a93327d303017afda3b01dd3f027
orangemug <info@orangemug.co.uk>
2019-10-27 20:04:57 +0000

    Update map style even if inspectModeEnabled=true

--- src/components/map/MapboxGlMap.jsx
+++ src/components/map/MapboxGlMap.jsx
@@ -87,11 +87,9 @@ export default class MapboxGlMap extends React.Component {
     const metadata = props.mapStyle.metadata || {}
     MapboxGl.accessToken = metadata['maputnik:mapbox_access_token'] || tokens.mapbox
 
-    if(!props.inspectModeEnabled) {
-      //Mapbox GL now does diffing natively so we don't need to calculate
-      //the necessary operations ourselves!
-      this.state.map.setStyle(props.mapStyle, { diff: true})
-    }
+    //Mapbox GL now does diffing natively so we don't need to calculate
+    //the necessary operations ourselves!
+    this.state.map.setStyle(props.mapStyle, {diff: true})
   }
 
   componentDidUpdate(prevProps) {
