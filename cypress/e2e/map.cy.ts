9c9d6bc7152d9a1ac6f3b68310e63a66234d2905
HarelM <harel.mazor@gmail.com>
2023-12-16 08:04:14 +0200

    Finish migration of tests

---
+++ cypress/e2e/map.cy.ts
@@ -0,0 +1,25 @@
+import driver from "./driver";
+
+describe("map", () => {
+    describe("zoom level", () => {
+      beforeEach(() => {
+        driver.beforeEach();
+      });
+      it("via url", () => {
+        var zoomLevel = 12.37;
+        driver.setStyle("geojson", zoomLevel);
+        driver.isDisplayedInViewport(".maplibregl-ctrl-zoom");
+        // HM TODO
+        //driver.getText(".maplibregl-ctrl-zoom") === "Zoom "+(zoomLevel);
+      })
+      it("via map controls", () => {
+        var zoomLevel = 12.37;
+        driver.setStyle("geojson", zoomLevel);
+  
+        driver.click(".maplibregl-ctrl-zoom-in");
+        driver.isDisplayedInViewport(".maplibregl-ctrl-zoom");
+        // HM TODO
+        //driver.getText(".maplibregl-ctrl-zoom") === "Zoom "+(zoomLevel + 1);
+      })
+    })
+  })
