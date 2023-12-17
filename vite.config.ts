93e71f755c026fe537a78727b5c09cb5d2f79c19
Harel M <harel.mazor@gmail.com>
2023-12-17 20:41:56 +0000

    Remove webpack config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    port: 8888
+  },
   plugins: [react()],
   define: {
     global: "window",
