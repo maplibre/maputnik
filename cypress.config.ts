4eb1ece6181d5a989edf72b068c23c61082f85d9
HarelM <harel.mazor@gmail.com>
2023-12-20 00:00:49 +0200

    Added retries to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
 });
