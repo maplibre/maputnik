50226a9b2ebf4c0f391143307cb318fbfeaa168a
HarelM <harel.mazor@gmail.com>
2023-12-21 12:00:19 +0200

    Fix build

--- src/components/IconLayer.jsx
+++ src/components/IconLayer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import IconLine from './IconLine.jsx'
-import IconFill from './IconFill.jsx'
-import IconSymbol from './IconSymbol.jsx'
-import IconBackground from './IconBackground.jsx'
-import IconCircle from './IconCircle.jsx'
-import IconMissing from './IconMissing.jsx'
+import IconLine from './IconLine'
+import IconFill from './IconFill'
+import IconSymbol from './IconSymbol'
+import IconBackground from './IconBackground'
+import IconCircle from './IconCircle'
+import IconMissing from './IconMissing'
 
 export default class IconLayer extends React.Component {
   static propTypes = {
