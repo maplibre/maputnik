cd91a51278732f5db8642cdc6180512daef62255
shelly_goldblit <shelly_goldblit@dell.com>
2023-12-26 08:16:01 +0200

    fix cypress fail in some browsers

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       require("@cypress/code-coverage/task")(on, config);
       return config;
     },
+    baseUrl: "http://127.0.0.1:8888",
     retries: {
       runMode: 2,
       openMode: 0,
