ca8a8e47729b2b544b1c4852e7718a093d5ab9e5
HarelM <harel.mazor@gmail.com>
2023-12-27 11:19:10 +0200

    Fix lint

--- cypress/e2e/thirs-party-driver.ts
+++ cypress/e2e/thirs-party-driver.ts
@@ -1,29 +1,29 @@
 export default class ThirdPartyDriver {
-    public given = {
-        /**
+  public given = {
+    /**
          * 
          * @param url a url to a file, this assumes the file name is the last part of the url
          * @param alias 
          */
-        interceptGetToFile(url: string) {
-            let fileNameAndAlias = url.split('/').pop();
-            cy.intercept('GET', url, { fixture: fileNameAndAlias }).as(fileNameAndAlias!);
-        },
+    interceptGetToFile(url: string) {
+      let fileNameAndAlias = url.split('/').pop();
+      cy.intercept('GET', url, { fixture: fileNameAndAlias }).as(fileNameAndAlias!);
+    },
 
-        interceptAndIgnore(url: string) {
-            cy.intercept({ method: "GET", url }, []);
-        }
+    interceptAndIgnore(url: string) {
+      cy.intercept({ method: "GET", url }, []);
     }
+  }
 
-    public get = {
-        element(slector: string) {
-            return cy.get(slector);
-        }
+  public get = {
+    element(slector: string) {
+      return cy.get(slector);
     }
+  }
 
-    public when = {
-        confirmAlert() {
-            cy.on("window:confirm", () => true);
-        }
+  public when = {
+    confirmAlert() {
+      cy.on("window:confirm", () => true);
     }
+  }
 }
