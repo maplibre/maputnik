e49d0c21bd1316abd8ef8c7bfd45349ec27cae43
Birk Skyum <birk.skyum@pm.me>
2025-01-21 23:42:01 +0100

    add test

--- cypress/e2e/layers.cy.ts
+++ cypress/e2e/layers.cy.ts
@@ -494,4 +494,33 @@ describe("layers", () => {
       ).shouldBeVisible();
     });
   });
+
+
+  describe.only("layereditor jsonlint should error", ()=>{
+    
+    it("add", () => {
+      const id = when.modal.fillLayers({
+        type: "circle",
+        layer: "example",
+      });
+
+      then(get.styleFromLocalStorage()).shouldDeepNestedInclude({
+        layers: [
+          {
+            id: id,
+            type: "circle",
+            source: "example",
+          },
+        ],
+      });
+
+      const sourceText = get.elementByText('"source"');
+      
+      sourceText.click();
+      sourceText.type("\"");
+      
+      const error = cy.get('.CodeMirror-lint-marker-error');
+      error.should('exist');
+    });
+  });
 });
