28991ed3289ef083e97add94b09c71c833950583
HarelM <harel.mazor@gmail.com>
2025-09-13 23:07:37 +0300

    Use different icons for different layers

--- src/components/IconLayer.tsx
+++ src/components/IconLayer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 import type {CSSProperties} from "react";
-import { BsDiamond, BsDiamondFill, BsFonts, BsSun } from "react-icons/bs";
-import { MdBubbleChart, MdOutlineCircle, MdPriorityHigh } from "react-icons/md";
+import { BsDiamondFill, BsFonts, BsSun } from "react-icons/bs";
+import { MdBubbleChart, MdOutlineCircle, MdOutlineStreetview, MdOutlineVerticalShades, MdPriorityHigh } from "react-icons/md";
 import { IoAnalyticsOutline } from "react-icons/io5";
+import { CiMountain1 } from "react-icons/ci";
 
 type IconLayerProps = {
   type: string
@@ -14,13 +15,13 @@ type IconLayerProps = {
 const IconLayer: React.FC<IconLayerProps> = (props) => {
   const iconProps = { style: props.style };
   switch(props.type) {
-    case "fill-extrusion": return <BsDiamondFill {...iconProps} />;
-    case "raster": return <BsDiamond {...iconProps} />;
+    case "fill-extrusion": return <MdOutlineVerticalShades {...iconProps} />;
+    case "raster": return <BsDiamondFill {...iconProps} />;
     case "hillshade": return <BsSun {...iconProps} />;
-    case "color-relief": return <MdBubbleChart {...iconProps} />;
-    case "heatmap": return <BsDiamond {...iconProps} />;
-    case "fill": return <BsDiamond {...iconProps} />;
-    case "background": return <BsDiamondFill {...iconProps} />;
+    case "color-relief": return <CiMountain1 {...iconProps} />;
+    case "heatmap": return <MdBubbleChart {...iconProps} />;
+    case "fill": return <BsDiamondFill {...iconProps} />;
+    case "background": return <MdOutlineStreetview {...iconProps} />;
     case "line": return <IoAnalyticsOutline {...iconProps} />;
     case "symbol": return <BsFonts {...iconProps} />;
     case "circle": return <MdOutlineCircle {...iconProps} />;
