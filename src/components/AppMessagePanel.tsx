0748e1b3bfb01278a9e7d6adcac6c558749f6b63
HarelM <harel.mazor@gmail.com>
2026-03-03 10:44:48 +0200

    Fix build

--- src/components/AppMessagePanel.tsx
+++ src/components/AppMessagePanel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { formatLayerId } from "../libs/format";
 import { type LayerSpecification, type StyleSpecification } from "maplibre-gl";
-import { Trans, type WithTranslation, withTranslation } from "react-i18next";
+import { type WithTranslation, withTranslation } from "react-i18next";
 import { type MappedError } from "../libs/definitions";
 
 type AppMessagePanelInternalProps = {
