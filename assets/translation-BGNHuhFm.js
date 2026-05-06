8e1a419909c307da8caa2a5ce21cf24a387270a7
HarelM <HarelM@users.noreply.github.com>
2026-05-06 14:12:36 +0000

    deploy: 93596b35405f7768b9783ae7f0d232b481c494c6

---
+++ assets/translation-BGNHuhFm.js
@@ -0,0 +1,2 @@
+const e={"Drag and drop a style JSON file here or click to browse":"Drag and drop a style JSON file here or click to browse"};export{e as default};
+//# sourceMappingURL=translation-BGNHuhFm.js.map
