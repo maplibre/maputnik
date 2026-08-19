ff50972c0d56209cdeaba3d4e58bdafef039e328
HarelM <HarelM@users.noreply.github.com>
2026-08-19 22:09:54 +0000

    deploy: 4424276073d155e149d325592e50d564e81a4378

--- assets/translation-BGNHuhFm.js
+++
@@ -1,2 +0,0 @@
-const e={"Drag and drop a style JSON file here or click to browse":"Drag and drop a style JSON file here or click to browse"};export{e as default};
-//# sourceMappingURL=translation-BGNHuhFm.js.map
---
+++ assets/translation-kg7OItc9.js
@@ -0,0 +1,2 @@
+var e={"Drag and drop a style JSON file here or click to browse":`Drag and drop a style JSON file here or click to browse`};export{e as default};
+//# sourceMappingURL=translation-kg7OItc9.js.map
